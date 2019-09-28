a3de2b371633fd7b8f4415f193fe434ce51c7f08
AvengerEug <eugenesumarry@163.com>
2019-09-28 13:05:08 +0800

    跨域的两个工具类去掉@Configration注解, 使用自定义@EnableCors注解import即可(原因: 因为不需要使用为这两个工具类使用代理对象创建, 并且工具类里面也没有@Bean注解创建的对象, 所以不需要@Configration注解)

--- src/main/java/com/eugene/common/web/mvc/MVCConfiguration.java
+++ src/main/java/com/eugene/common/web/mvc/MVCConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
 public class MVCConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
--- src/main/java/com/eugene/common/web/security/CorsConfigration.java
+++ src/main/java/com/eugene/common/web/security/CorsConfigration.java
@@ -2,14 +2,11 @@ package com.eugene.common.web.security;
 
 import com.eugene.common.constants.FilterOrderConstants;
 import com.eugene.common.factory.FilterFactory;
-import com.eugene.common.filter.JwtAuthFilter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration
 public class CorsConfigration extends WebSecurityConfigurerAdapter {
 
     @Override
